0e3f151fef3b78e2fd6e8f107962f8672e13cd17
Chakiel Zero YouTube <136051930+Chakielzero@users.noreply.github.com>
2024-12-30 10:58:29 -0400

    Update listas.js

--- sudachi/assets/js/listas.js
+++ sudachi/assets/js/listas.js
@@ -1,37 +1,5 @@
 // Lista de juegos
-const juegos = [
-    {
-        id: "3rdbirtday",
-        imgSrc: "assets/img/3rd.jpg",
-        altText: "3rdbirtday",
-        title: "3rdbirtday.cso",
-        downloadLink2: "https://nephobox.com/s/1ZKJ7N0rNTA68toQfJQMS6w" // Segundo enlace de descarga
-
-    },       
-    {
-        id: "Ben 10 - Protector of Earth",
-        imgSrc: "assets/img/ben10.jpg",
-        altText: "Ben 10 - Protector of Earth",
-        title: "Ben 10 - Protector of Earth.iso",
-        downloadLink2: "https://nephobox.com/s/1nLVW9iBaX8nfLYX5HCMLjg" // Segundo enlace de descarga
-
-    },       
-    {
-        id: "Burnout Legends",
-        imgSrc: "assets/img/burnout.jpg",
-        altText: "Burnout Legends",
-        title: "Burnout Legends.iso",
-        downloadLink2: "https://nephobox.com/s/1baGLDXJ0lLOl5vFPZ3Re4w" // Segundo enlace de descarga
-
-    },       
-    {
-        id: "Cars",
-        imgSrc: "assets/img/ben10.jpg",
-        altText: "Ben 10 Protector Of Earth"",
-        title: "Cars (Spain).iso",
-        downloadLink2: "https://nephobox.com/s/1aBD6Ts6lJgsQ6RgoYtGgwg" // Segundo enlace de descarga
-
-    },       
+const juegos = [      
     {
         id: "Cadence of Hyrule Crypt",
         imgSrc: "assets/img/cadenceofhyrule.jpg",
